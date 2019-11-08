e2bebd2913dbd49bbcb7d46b68390c2c11e6e2c7
Daxter Onyx <daxter5onyx@gmail.com>
2019-11-08 22:46:40 +0100

    update unity

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+	private static InventoryManager m_instance;
+	public static InventoryManager Instance
+	{
+		get { return m_instance; }
+	}
+}
