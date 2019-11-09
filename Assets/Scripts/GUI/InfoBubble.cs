910f84538c479371b488c5d0115e1a9a93c1fcb4
Leon <romain.delpy@hotmail.com>
2019-11-09 06:54:21 +0100

    Base bulle pop-up

---
+++ Assets/Scripts/GUI/InfoBubble.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoBubble : MonoBehaviour
+{
+    public Vector3 gap;
+    
+    void Update()
+    {
+        transform.position = Input.mousePosition + gap;
+    }
+}
