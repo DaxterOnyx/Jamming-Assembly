ab1c897c9a8d3115103271316b8c3fd994fb4f75
Leon <romain.delpy@hotmail.com>
2019-11-09 11:03:10 +0100

    PLayer -> PlayerManager

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,22 +1,22 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerManager : MonoBehaviour
 {
-    private static Player m_instance;
-    public static Player Instance
+    private static PlayerManager m_instance;
+    public static PlayerManager Instance
     {
         get
         {
             if (m_instance == null)
             {
-                m_instance = FindObjectOfType<Player>();
+                m_instance = FindObjectOfType<PlayerManager>();
             }
             if (m_instance == null)
             {
                 GameObject newInstance = Instantiate(new GameObject());
                 newInstance.name = "Player (Singleton)";
-                m_instance = newInstance.AddComponent<Player>();
+                m_instance = newInstance.AddComponent<PlayerManager>();
             }
             return m_instance;
         }
