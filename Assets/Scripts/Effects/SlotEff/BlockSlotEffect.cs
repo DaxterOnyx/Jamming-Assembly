adb9a02ca8f9ab66952048d51fbb5923cd85a626
Daxter Onyx <daxter5onyx@gmail.com>
2019-11-10 18:37:26 +0100

    effect error de ses morts

--- Assets/Scripts/Effects/SlotEff/BlockSlotEffect.cs
+++ Assets/Scripts/Effects/SlotEff/BlockSlotEffect.cs
@@ -1,13 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BlockSlotEffect : SlotEffect
+﻿public class BlockSlotEffect : SlotEffect
 {
-    internal override void ApplyEffect(Slot slot)
-    {
+	internal override void ApplyEffect(Slot slot)
+	{
 		var item = slot.GetItem();
-		if (item == null)
+		if (item != null)
 			item.Lock();
 		else
 			slot.SetState(Slot.State.LOCKED);
