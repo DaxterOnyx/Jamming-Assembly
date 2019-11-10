a5b3325be885ecfcea08709f6fa6799baedd3b88
Daxter Onyx <daxter5onyx@gmail.com>
2019-11-10 19:09:50 +0100

    tuto et background press to play

---
+++ Assets/TutoManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class TutoManager : MonoBehaviour
+{
+	int index = 0;
+	public GameObject[] gameObjects;
+	void Update()
+	{
+		if (Input.anyKeyDown && index < gameObjects.Length) {
+			gameObjects[index++].SetActive(false);
+			if (index < gameObjects.Length)
+				gameObjects[index].SetActive(true);
+		}
+	}
+}
