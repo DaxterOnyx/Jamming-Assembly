9c1a71035aac0a1dcd921f63ff6580b5061e9842
Digzeblue <46719379+Digzeblue@users.noreply.github.com>
2019-11-10 08:48:43 +0100

    Implémentation son (ajout fmod)

---
+++ Assets/Scripts/Sound/footstepSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class footstepSound : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string inputsound;       //Sound to play
+    bool ismoving;        
+    public float walkingspeed;
+
+
+    void Update()
+    {
+        ismoving = true;           //TO DO : Améliorer les conditions sur ismoving
+    }
+
+    void callFootsteps ()
+    {
+    if (ismoving == true) { 
+            FMODUnity.RuntimeManager.PlayOneShot(inputsound);
+        }
+    }
+
+    void Start()
+    {
+        InvokeRepeating("callFootsteps", 0, walkingspeed);  //les bruits de pas ne peuvent se répéter qu'avec walkingspeed d'intervalle
+    }
+}
