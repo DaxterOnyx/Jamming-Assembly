10ebd3c02dbad47f9e9ed4ff7bf133b26cb72400
Leon <romain.delpy@hotmail.com>
2019-11-10 08:47:14 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/CorridorScript/UIGridData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "UIGridData", menuName = "ScriptableObjects/UIGridData")]
+public class UIGridData : ScriptableObject
+{
+    public GameObject uiCell;
+    public float distanceBetweenCell;
+    public float cellSize;
+}
---
+++ Assets/Scripts/GUI/UIItemGrid.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIItemGrid : MonoBehaviour
+{
+    public UIGridData data;
+    private GameObject[,] uiCellTab;
+    private Vector2Int gridSize;
+    internal void setGridSize(Vector2Int itemSize)
+    {
+        
+    }
+
+    internal void DestroyCells()
+    {
+        foreach (var item in uiCellTab)
+        {
+        }
+    }
+}
