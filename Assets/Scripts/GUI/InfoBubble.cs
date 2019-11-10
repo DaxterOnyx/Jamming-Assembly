b827d06d677ff97bab42c04af8002aa6ef68c991
Leon <romain.delpy@hotmail.com>
2019-11-10 16:08:49 +0100

    Stabilisation info bubble

--- Assets/Scripts/GUI/InfoBubble.cs
+++ Assets/Scripts/GUI/InfoBubble.cs
@@ -1,55 +1,85 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class InfoBubble : MonoBehaviour
 {
-	private static InfoBubble m_instance;
-	public static InfoBubble Instance
-	{
-		get {
-			if (m_instance == null)
-				m_instance = FindObjectOfType<InfoBubble>();
-			if (m_instance == null) {
-				Debug.LogError("Singleton InfoBubble not found");
-
-			}
-
-			return m_instance;
-		}
-	}
-
-	public Vector3 gapMouseBubble;
-
-	internal void Init()
-	{
-		m_instance = this;
-	}
-
-	void Update()
+    private static InfoBubble m_instance;
+    public static InfoBubble Instance
     {
-		UpdatePosition();
+        get
+        {
+            if (m_instance == null)
+                m_instance = FindObjectOfType<InfoBubble>();
+            if (m_instance == null)
+            {
+                Debug.LogError("Singleton InfoBubble not found");
+
+            }
+
+            return m_instance;
+        }
     }
 
-	private void UpdatePosition()
-	{
-		transform.position = Input.mousePosition + gapMouseBubble;
-	}
+    private RectTransform rect;
+    public Vector2 screenSize;
+    public Vector3 gapMouseBubble;
 
-	public void Show(Item item)
-	{
-		UpdatePosition();
+    internal void Init()
+    {
+        rect = this.GetComponent<RectTransform>();
+        m_instance = this;
+    }
+
+    void Update()
+    {
+        UpdatePosition();
+        ChangePivot();
+    }
+
+    private void UpdatePosition()
+    {
+        transform.position = Input.mousePosition + gapMouseBubble;
+    }
+
+    private void ChangePivot()
+    {
+        float yPositionMax = rect.position.y + rect.sizeDelta.y;
+        float yPositionMin = rect.position.y - rect.sizeDelta.y;
+        float xPositionMax = rect.position.x + rect.sizeDelta.x;
+        float xPositionMin = rect.position.x - rect.sizeDelta.x;
+        Debug.Log(rect.position);
+        if (yPositionMax> screenSize.y)
+        {
+            rect.pivot = new Vector2(rect.pivot.x, 1);
+        }
+        else if (yPositionMin < 0)
+        {
+            rect.pivot = new Vector2(rect.pivot.x, 0);
+        }
+
+        if (xPositionMax > screenSize.x)
+        {
+            rect.pivot = new Vector2(1 , rect.pivot.y);
+        }
+        else if (xPositionMin <0)
+        {
+            rect.pivot = new Vector2(0, rect.pivot.y);
+        }
+    }
+
+    public void Show(Item item)
+    {
+        UpdatePosition();
         //TODO Show Data
         GetComponentInChildren<UIItemGrid>()?.setGridSize(item.size);
-		gameObject.SetActive(true);
-	}
+        gameObject.SetActive(true);
+    }
 
-	public void Hide()
-	{
-		gameObject.SetActive(false);
+    public void Hide()
+    {
+        gameObject.SetActive(false);
         GetComponentInChildren<UIItemGrid>()?.RestartCell();
     }
 
-	
+
 }
