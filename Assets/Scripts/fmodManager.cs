69363fbadb186e9412e96d86bb375ffef36e676b
Daxter Onyx <daxter5onyx@gmail.com>
2019-11-10 10:48:30 +0100

    merge

---
+++ Assets/Scripts/fmodManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fmodManager : MonoBehaviour
+
+{
+    private static fmodManager m_instance;
+    public static fmodManager Instance
+    {
+        get
+        {
+            if (m_instance == null)
+                m_instance = FindObjectOfType<fmodManager>();
+            if (m_instance == null)
+            {
+                GameObject newInstance = Instantiate(new GameObject());
+                newInstance.name = "fmodManager (Singleton)";
+                m_instance = newInstance.AddComponent<fmodManager>();
+            }
+
+            return m_instance;
+        }
+    }
+
+    public SoundData soundData;
+
+    public void OnChangeDragging()
+    {
+        string inputsound = soundData.inputsound;
+        FMODUnity.RuntimeManager.PlayOneShot(inputsound);
+    }
+}
